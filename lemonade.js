95503b4c101f2647d7661dbd352959d68719e8d4
arainmehmood4 <69026330+arainmehmood4@users.noreply.github.com>
2020-09-05 08:56:24 -0700

    Create lemonade.js

---
+++ lemonade.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+    let five = 0, ten = 0;
+    for ( let i of bills ) {
+        if ( i == 5 ) {
+            five++;
+        }
+        else if ( i == 10 ) { 
+            five--;
+            ten++;
+        }
+        else if ( ten > 0 ) {
+            ten--;
+            five--;
+        }
+        else {
+            five -= 3;            
+        }
+        if ( five < 0 ) 
+            return false
+    }
+    return true
+};
